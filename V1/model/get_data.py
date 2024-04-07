193d8c71684c0d45e39701472ed226bb8f9e4fab
Koijikun <105658234+Koijikun@users.noreply.github.com>
2024-04-07 13:18:42 +0200

    Update get_data.py

--- V1/model/get_data.py
+++ V1/model/get_data.py
@@ -8,23 +8,18 @@ def get_mongo_data():
     parser.add_argument('-u', '--uri', required=True, help="mongodb uri with username/password")
     args = parser.parse_args()
 
-    # MongoDB connection parameters
     mongo_uri = args.uri
     mongo_db = "projectone"
     mongo_collection = "anime"
 
-    # Connect to MongoDB
     client = MongoClient(mongo_uri)
     db = client[mongo_db]
     collection = db[mongo_collection]
 
-    # Fetch data from MongoDB
     cursor = collection.find({})
     data = list(cursor)
 
-    # Convert data to DataFrame
     df = pd.DataFrame(data)
 
-    # Display DataFrame
     return(df)
 
